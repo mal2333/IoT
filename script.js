60eec592e0179fb66f96ac4215788d8b9eb128f2
mal2333 <65696955+mal2333@users.noreply.github.com>
2020-11-26 21:13:21 +0400

    update11/26

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
   accordion: false
 });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems);
+});
